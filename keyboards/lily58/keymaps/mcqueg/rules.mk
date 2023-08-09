f02f365406e430297eb0b7a0c2ac2e6af5fe8083
mcqueg <garrettmccue@gmail.com>
2023-08-08 23:53:44 -0500

    via enable

--- keyboards/lily58/keymaps/mcqueg/rules.mk
+++ keyboards/lily58/keymaps/mcqueg/rules.mk
@@ -1,6 +1,6 @@
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes        # Mouse keys
-EXTRAKEY_ENABLE = no        # Audio control and System control
+EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no
@@ -9,6 +9,9 @@ AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_ENABLE= yes     # OLED display
+VIA_ENABLE = yes
+OLED_DRIVER = SSD1306
+
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/layer_state_reader.c \
