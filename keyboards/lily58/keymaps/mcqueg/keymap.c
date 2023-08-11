cbff3331977873de319b2407beffe75340f50bc3
mcqueg <garrettmccue@gmail.com>
2023-08-11 00:54:04 -0500

    remove key logging from oled

--- keyboards/lily58/keymaps/mcqueg/keymap.c
+++ keyboards/lily58/keymaps/mcqueg/keymap.c
@@ -118,8 +118,8 @@ bool oled_task_user(void) {
         break;
     }
     //oled_write_ln(read_layer_state(), false);
-    oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
+    // oled_write_ln(read_keylog(), false);
+    // oled_write_ln(read_keylogs(), false);
     //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
     //oled_write_ln(read_host_led_state(), false);
     //oled_write_ln(read_timelog(), false);
