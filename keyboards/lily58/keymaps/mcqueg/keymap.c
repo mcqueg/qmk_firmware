23225a768cad323fcc59a67acee44fed2e52d889
mcqueg <garrettmccue@gmail.com>
2023-08-11 19:42:35 -0500

    remap nav

--- keyboards/lily58/keymaps/mcqueg/keymap.c
+++ keyboards/lily58/keymaps/mcqueg/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_GRV,
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_QUOT,
   KC_LCTL,  KC_A,   KC_R,    ALT_T(KC_S), LCMD_T(KC_T), KC_G,              KC_M,    RCMD_T(KC_N),    KC_E,    KC_I,    KC_O,  KC_ENT,   
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,   KC_V,   KC_ENT,      _______,   KC_K,    KC_H,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,   KC_V,   KC_ENT,      _______, KC_K,  KC_H, KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
                     _______, _______, TT(_NAV_MAC), SFT_T(KC_SPC), SFT_T(KC_BSPC), LT(_SYM,KC_DEL), _______, _______
 ),
 [_NAV_MAC] = LAYOUT(
@@ -48,7 +48,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_LBRC,   KC_RBRC, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
 //            TT(_MOUSE), KC_LOPT, KC_LCMD, LT(_SYM_FN,KC_SPACE), LT(_NUM_NAV,KC_ENT), KC_RCMD, KC_RCTL, KC_RALT
 // ),
-
  
 // [_SYM_FN] = LAYOUT(
 //   _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
